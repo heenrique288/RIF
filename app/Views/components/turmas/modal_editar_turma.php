21c8fb6a93f00558112d4bb77b1de1f6b31a2a81
IsabellaT29 <115107520+IsabellaT29@users.noreply.github.com>
2025-08-28 22:09:15 -0400

    fix: corrigindo rota da action dos form e adicionando o jquery

--- app/Views/components/turmas/modal_editar_turma.php
+++ app/Views/components/turmas/modal_editar_turma.php
@@ -1,39 +1,63 @@
-<div class="modal fade modal-xl" id="modal-editar-turma" tabindex="-1" role="dialog" aria-labelledby="modal-editar-turma-label" aria-hidden="true">
+<div class="modal fade" id="modal-editar-turma" tabindex="-1" role="dialog" aria-labelledby="modal-editar-turma-label" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
+
             <div class="modal-header">
                 <h5 class="modal-title" id="modal-editar-turma-label">Editar Turma</h5>
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
-            <form id="editarTurma" class="forms-sample" method="post" action="<?= base_url('sys/turmas/atualizar') ?>">
+
+            <form id="editarTurma" class="forms-sample" method="post" action="<?= base_url('sys/turmas/update') ?>">
+                <?= csrf_field() ?>
+
                 <div class="modal-body">
-                    <?= csrf_field() ?>
+                    
                     <input type="hidden" name="id" id="editTurmaId" />
-                    <div class="row">
-                        <div class="form-group">
-                            <label for="edit-nome">Nome</label>
-                            <input type="text" class="form-control" required
-                                id="edit-nome" name="nome" placeholder="Insira o nome da Turma">
-                        </div>
-                        <div class="col-md-6 col-sm-12">
-                            <div class="form-group">
-                                <label for="edit-curso_id">Curso</label>
-                                <select class="form-select" id="edit-curso_id" name="curso_id" required>
-                                    <?php foreach ($cursos as $curso): ?>
-                                        <option value="<?= esc($curso['id']) ?>"><?= esc($curso['nome']) ?></option>
-                                    <?php endforeach; ?>
-                                </select>
-                            </div>
-                        </div>
+
+                    
+                    <div class="form-group">
+                        <label for="edit-nome">Nome</label>
+                        <input type="text" class="form-control" required
+                            id="edit-nome" name="nome" placeholder="Insira o nome da Turma">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="edit-curso_id">Curso</label>
+                        <select class="form-select" id="edit-curso_id" name="curso_id" required>
+                            <?php foreach ($cursos as $curso): ?>
+                                <option value="<?= esc($curso['id']) ?>"><?= esc($curso['nome']) ?></option>
+                            <?php endforeach; ?>
+                        </select>
                     </div>
                 </div>
+
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                     <button type="submit" class="btn btn-primary">Salvar</button>
                 </div>
             </form>
+
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+
+        $("#modal-editar-turma").on("show.bs.modal", function(event) {
+            var button = $(event.relatedTarget);
+
+            var nome = button.data("nome");
+            var id = button.data("id");
+            var curso_id = button.data("curso_id");
+
+            var modal = $(this);
+            modal.find("#editTurmaId").val(id);
+            modal.find("#edit-nome").val(nome);
+            modal.find("#edit-curso_id").val(curso_id);
+        });
+
+    });
+</script>

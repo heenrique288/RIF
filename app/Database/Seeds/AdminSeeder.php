85058feec748608e0f006edcfb07a173899d6182
Ana Clara Gibson <anaclarasouzagibson@gmail.com>
2025-08-30 20:27:49 -0300

    feat: Adiciona o Seeder para o login com usuário Admin.

---
+++ app/Database/Seeds/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Shield\Entities\User;
+use CodeIgniter\Shield\Models\UserModel;
+use CodeIgniter\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        $adminPassword = env('admin.defaultPassword', 'admin');
+
+        $users = model(UserModel::class);
+
+        $user = new User([
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => $adminPassword,
+        ]);
+
+        $users->save($user);
+    }
+}

c72a5a7124e26802e280e6ad97a1a910894f5431
tenio7355 <estenio.silva.castro@gmail.com>
2025-09-05 13:30:25 -0400

    Feat: Adição da Funcionalidade de DataTable na tela de Agendamento de Refeição

--- app/Views/sys/agendamento.php
+++ app/Views/sys/agendamento.php
@@ -1,6 +1,5 @@
 <?php echo view('components/agendamentos/modal_cadastrar_agendamento', ["turmas" => $turmas], ["alunos" => $alunos]) ?>
 
-
 <h1>Agendamento de Refeição</h1>
 <div class="my-4">
     <button type="button" class="btn btn-primary btn-fw" data-bs-toggle="modal" data-bs-target="#modal-cadastrar-agendamento">
@@ -9,6 +8,7 @@
     </button>
 </div>
 
+<?php if (isset($agendamentos) && !empty($agendamentos)): ?>
 <table class="table mb-4" id="listagem-agendamentos">
     <thead>
         <tr>
@@ -20,50 +20,13 @@
             <th>Ações</th>
         </tr>
     </thead>
-
     <tbody>
-        <tr>
-            <td>Exemplo de exibição do nome : Joao</td>
-            <td>Exemplo de exibição da data : 2025-08-27</td>
-            <td>123456</td>
-            <td>7890</td>
-            <td>Refeição especial</td>
-            <td>
-                <div class="d-flex">
-                    <!-- Editar -->
-                    <span data-bs-toggle="tooltip" title="Editar agendamento">
-                        <button type="button"
-                            class="justify-content-center align-items-center d-flex btn btn-inverse-success button-trans-success btn-icon me-1"
-                            data-bs-toggle="modal"
-                            data-bs-target="#modal-agendamento"
-                            data-id="1"
-                            data-data="2025-08-27"
-                            data-turma="Turma A"
-                            data-alunos='["João","Maria"]'>
-                            <i class="fa fa-edit"></i>
-                        </button>
-                    </span>
-
-                    <!-- Excluir -->
-                    <span data-bs-toggle="tooltip" title="Excluir agendamento">
-                        <button type="button"
-                            class="justify-content-center align-items-center d-flex btn btn-inverse-danger button-trans-danger btn-icon"
-                            data-bs-toggle="modal"
-                            data-bs-target="#modal-deletar-agendamento"
-                            data-id="1">
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    </span>
-                </div>
-            </td>
-
-        </tr>
     </tbody>
-
 </table>
+<?php else: ?>
+    <p>Nenhum agendamento encontrado no banco de dados.</p>
+<?php endif; ?>
 
-
-<!-- Modal para ver todos os alunos -->
 <div class="modal fade" id="modal-ver-alunos" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -78,7 +41,6 @@
     </div>
 </div>
 
-<!-- Modal para ver justificativa -->
 <div class="modal fade" id="modal-ver-justificativa" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -93,14 +55,10 @@
     </div>
 </div>
 
-
 <style>
-    /* Estilo da lista de alunos selecionados */
     #lista-alunos-modal li {
         background-color: #2a3038;
-        /* Azul clarinho */
         color: #ffffff;
-        /* Texto preto para melhor contraste */
         display: flex;
         justify-content: space-between;
         align-items: center;
@@ -109,3 +67,116 @@
         margin-bottom: 0.25rem;
     }
 </style>
+
+<script>
+    const dataTableLangUrl = "<?php echo base_url('assets/js/traducao-dataTable/pt_br.json'); ?>";
+    const agendamentosData = <?= json_encode($agendamentos ?? []) ?>;
+
+    $(document).ready(function() {
+        const initTooltips = () => {
+            $('[data-bs-toggle="tooltip"]').each(function() {
+                const tooltipInstance = bootstrap.Tooltip.getInstance(this);
+                if (tooltipInstance) {
+                    tooltipInstance.dispose();
+                }
+                new bootstrap.Tooltip(this, {
+                    container: 'body',
+                    customClass: 'tooltip-on-top',
+                    offset: [0, 10]
+                });
+            });
+        };
+
+        if (agendamentosData.length > 0) {
+            $('#listagem-agendamentos').DataTable({
+                data: agendamentosData,
+                columns: [{
+                    data: 'turma_aluno',
+                    render: function(data, type, row) {
+                        if (row.alunos && row.alunos.length > 1) {
+                            return `${row.alunos[0]} <a href="#" class="ver-mais-alunos" data-alunos='${JSON.stringify(row.alunos)}'> + ${row.alunos.length - 1} alunos</a>`;
+                        } else if (row.alunos && row.alunos.length === 1) {
+                            return row.alunos[0];
+                        }
+                        return data;
+                    }
+                }, {
+                    data: 'data'
+                }, {
+                    data: 'crc'
+                }, {
+                    data: 'codigo'
+                }, {
+                    data: 'justificativa',
+                    render: function(data, type, row) {
+                        return `<a href="#" class="ver-justificativa" data-justificativa='${data.replace(/'/g, "\\'")}'>Ver justificativa</a>`;
+                    }
+                }, {
+                    data: null,
+                    orderable: false,
+                    searchable: false,
+                    render: function(data, type, row) {
+                        return `
+                            <div class="d-flex">
+                                <span data-bs-toggle="tooltip" data-placement="top" title="Editar agendamento">
+                                    <button
+                                        type="button"
+                                        class="justify-content-center align-items-center d-flex btn btn-inverse-success button-trans-success btn-icon me-1">
+                                        <i class="fa fa-edit"></i>
+                                    </button>
+                                </span>
+                                <span data-bs-toggle="tooltip" data-placement="top" title="Excluir agendamento">
+                                    <button
+                                        type="button"
+                                        class="justify-content-center align-items-center d-flex btn btn-inverse-danger button-trans-danger btn-icon me-1">
+                                        <i class="fa fa-trash"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        `;
+                    }
+                }],
+                language: {
+                    search: "Pesquisar:",
+                    url: dataTableLangUrl
+                },
+                ordering: true,
+                aLengthMenu: [
+                    [10, 25, 50, -1],
+                    [10, 25, 50, "Todos"],
+                ],
+                initComplete: function(settings, json) {
+                    initTooltips();
+                },
+                drawCallback: function() {
+                    initTooltips();
+                }
+            });
+        }
+    });
+
+    // Lógica de notificação
+    <?php if (session()->has('erros')): ?>
+        <?php foreach (session('erros') as $erro): ?>
+            $.toast({
+                heading: 'Erro',
+                text: '<?= esc($erro); ?>',
+                showHideTransition: 'fade',
+                icon: 'error',
+                loaderBg: '#dc3545',
+                position: 'top-center'
+            });
+        <?php endforeach; ?>
+    <?php endif; ?>
+
+    <?php if (!session()->has('erros') && session()->has('sucesso')): ?>
+        $.toast({
+            heading: 'Sucesso',
+            text: '<?= session('sucesso') ?>',
+            showHideTransition: 'fade',
+            icon: 'success',
+            loaderBg: '#35dc5fff',
+            position: 'top-center'
+        });
+    <?php endif; ?>
+</script>

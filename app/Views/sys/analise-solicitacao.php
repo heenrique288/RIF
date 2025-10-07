e633345b18fa55e5bc22bef4c304b4cb2f5b431d
tenio7355 <estenio.silva.castro@gmail.com>
2025-10-06 20:08:18 -0400

    Style: Padronizando a tela de Analise de Solicitações

--- app/Views/sys/analise-solicitacao.php
+++ app/Views/sys/analise-solicitacao.php
@@ -1,40 +1,44 @@
-<div>
-
-    <h1>Análise das Solicitações</h1>
-
+<div class="mb-3">
+    <h2 class="card-title mb-0">Análise das Solicitações</h2>
     <p>Aceite ou Recuse Solicitações de Refeições</p>
+</div>
+<div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-striped table-hover mb-4">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Código</th>
+                            <th>Turma</th>
+                            <th>Data</th>
+                            <th>CRC</th>
+                            <th>Código da Refeição</th>
+                            <th style="text-align: center; width:15%; min-width: 200px;"><strong>Ações</strong></th>
+                        </tr>
+                    </thead>
 
-    <table class="table table-striped table-hover mb-4">
-        <thead class="table-dark">
-            <tr>
-                <th>Código</th>
-                <th>Turma</th>
-                <th>Data</th>
-                <th>CRC</th>
-                <th>Código da Refeição</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>101</td>
-                <td>28/09/2025</td>
-                <td>12345</td>
-                <td>R001</td>
-                <td>
-                    <div class="d-flex">
-                        <button class="btn btn-success btn-sm me-2">
-                            <i class="fa fa-check me-1"></i> Aceitar
-                        </button>
-                        <button class="btn btn-danger btn-sm">
-                            <i class="fa fa-times me-1"></i> Recusar
-                        </button>
-                    </div>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>101</td>
+                            <td>28/09/2025</td>
+                            <td>12345</td>
+                            <td>R001</td>
+                            <td>
+                                <div class="d-flex">
+                                    <button class="btn btn-success btn-sm me-2">
+                                        <i class="fa fa-check me-1"></i> Aceitar
+                                    </button>
+                                    <button class="btn btn-danger btn-sm">
+                                        <i class="fa fa-times me-1"></i> Recusar
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>

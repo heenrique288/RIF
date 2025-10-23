9e5a61d724c4680a8e66b44bac389454356dd6bf
IsabellaT29 <115107520+IsabellaT29@users.noreply.github.com>
2025-10-22 22:22:26 -0400

    refactor: add input para o campo codigo

--- app/Controllers/TurmaController.php
+++ app/Controllers/TurmaController.php
@@ -37,6 +37,7 @@ class TurmaController extends BaseController
     {
         $post = $this->request->getPost();
 
+        $input['codTurma'] = strip_tags($post['codigo']);
         $input['nome'] = strip_tags($post['nome']);
         $input['curso_id'] = (int) strip_tags($post['curso_id']);
 
@@ -63,6 +64,7 @@ class TurmaController extends BaseController
         $post = $this->request->getPost();
 
         $input['id'] = (int) strip_tags($post['id']);
+        $input['codTurma'] = strip_tags($post['codTurma']);
         $input['nome'] = strip_tags($post['nome']);
         $input['curso_id'] = (int) strip_tags($post['curso_id']);
 
